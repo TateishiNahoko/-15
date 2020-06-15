2ec969ad3d372e98611166bed2d2d7f7755bd521
jeongyunji <P190954@cresco.co.jp>
2020-06-15 17:45:21 +0900

    gg

---
+++ 第二反復実装/Tic_tac_toe.java
@@ -0,0 +1,31 @@
+package miniproject_1;
+
+public class Tic_tac_toe {
+
+	public Tic_tac_toe() {
+		Turn turn = new Turn(Player player);
+	}
+	
+	public static void startTurn() {
+		
+	}
+	public void startGame() {
+		
+	}
+	
+	public boolean judge() {
+		
+	}
+	
+	public void showBoard() {
+		
+	}
+	
+	public void showFinish() {
+		
+	}
+	
+	public void changePlayer() {
+		
+	}
+}
