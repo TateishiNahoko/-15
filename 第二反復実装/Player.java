a03a04ba7d4ae1919623ab347a86302c2b2fbc68
jeongyunji <P190954@cresco.co.jp>
2020-06-15 13:47:10 +0900

    e

---
+++ 第二反復実装/Player.java
@@ -0,0 +1,30 @@
+package miniproject_1;
+
+import java.util.Scanner;
+
+public class Player {
+	private Mark mark;
+	private String name;
+	
+	
+	public Player(String name, Mark mark) {
+		this.name = name;
+		this.mark = mark;
+	}
+    public int[] selectCell() {
+    	int[] xy = new int[2];
+    	Scanner scanner = new Scanner(System.in);
+    	String line = scanner.nextLine();
+		String[] zahyo = line.split(" ");
+		xy[0] = Integer.parseInt(zahyo[0]);
+		xy[1] = Integer.parseInt(zahyo[1]);
+    	
+    
+        return xy;
+    }
+ 
+    public String getName() {
+    	return name;
+    }
+
+}
